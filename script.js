5b69427c17d3debf955ab90514bc4b1882955144
Agnès Cappello <senga.ds@gmail.com>
2024-08-07 20:50:55 +0200

    k7-v1

---
+++ script.js
@@ -0,0 +1,9 @@
+const idK7Button = document.getElementById("idK7Button");
+const ruban = document.getElementById("ruban");
+let currentBorderLeft = 10;
+
+idK7Button.addEventListener("click", allezGO);
+
+function allezGO() {
+  console.log("VAZY LA K7");
+}
